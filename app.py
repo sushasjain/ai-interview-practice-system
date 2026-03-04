8837aa467771b5cf162311e3b0ec7abce9bb58b5
susha <sushajain358@gmail.com>
2026-03-04 20:05:43 +0530

    Fix Gemini model

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import google.generativeai as genai
 # -----------------------------
 # Configure Gemini API
 # -----------------------------
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=os.getenv("AIzaSyC3Jo9s5_Rs1A5RlUbcp_OPyzG_HkNgVJU"))
 model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 # -----------------------------
